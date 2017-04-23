d22bcee752cc791104816a945781fe916ac826fc
samuvait <samu.vaittinen@helsinki.fi>
2017-04-23 23:58:41 +0300

    tehtava 5 6 7 8

---
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -0,0 +1,98 @@
+package ohtu;
+
+public class TennisGame {
+
+    private int m_score1 = 0;
+    private int m_score2 = 0;
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName.equals(this.player1Name)) {
+            m_score1++;
+        } else {
+            m_score2++;
+        }
+    }
+
+    public String getScore() {
+        String score = "";
+        int limit = 4;
+        if (m_score1 == m_score2) {
+            score = scoresEqual();
+        } else if (m_score1 >= limit || m_score2 >= limit) {
+            score = scoresHigherFour();
+        } else {
+            score = scoresNormal();
+        }
+        return score;
+    }
+
+    public String scoresNormal() {
+        String score = "";
+        score = getResult(m_score1) + "-" + this.getResult(this.m_score2);
+        return score;
+    }
+    
+    public String getResult(int tempScore) {
+        String score = "";
+        switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
+        }
+        return score;
+    }
+
+    public String scoresHigherFour() {
+        String score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            score = "Advantage player1";
+        } else if (minusResult == -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
+        }
+        return score;
+    }
+
+    public String scoresEqual() {
+        String score;
+        switch (m_score1) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+}
